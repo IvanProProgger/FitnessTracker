6f786053ee92a9422311f3f3200762bb9c9bc098
Иван Шелухин <i.shelukhin@internet.ru>
2023-03-24 22:16:14 +0300

    add main

---
+++ main.py
@@ -0,0 +1,116 @@
+class InfoMessage:
+    """Информационное сообщение о тренировке."""
+
+    def __init__(self, training_type, duration, distance, speed, calories):
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def get_message(self):
+        return f'Тип тренировки: {self.training_type}; Длительность: {self.duration} ч.; Дистанция: {self.distance} км; Ср. скорость: {self.speed} км/ч; Потрачено ккал: {self.calories}.'
+
+
+class Training:
+    """Базовый класс тренировки."""
+    M_IN_KM = 1000
+    LEN_STEP = 0.65
+    MIN_IN_HOUR = 60
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км."""
+        return self.action * self.LEN_STEP / self.M_IN_KM
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
+        return self.get_distance() / self.duration
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        pass
+
+    def show_training_info(self) -> InfoMessage:
+        """Вернуть информационное сообщение о выполненной тренировке."""
+        return InfoMessage(self.__class__.__name__, self.duration, self.get_distance(), self.get_mean_speed(),
+                           self.get_spent_calories())
+
+
+class Running(Training):
+    """Тренировка: бег."""
+    LEN_STEP = 0.65
+    CALORIES_MEAN_SPEED_MULTIPLIER = 18
+    CALORIES_MEAN_SPEED_SHIFT = 1.79
+
+    def __init__(self, action, duration, weight):
+        super().__init__(action, duration, weight)
+
+    def get_spent_calories(self):
+        return ((
+                            self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT) * self.weight / self.M_IN_KM * self.duration * self.MIN_IN_HOUR)
+
+
+class SportsWalking(Training):
+    """Тренировка: спортивная ходьба."""
+    LEN_STEP = 0.65
+    SportsWalking_coef_1 = 0.035
+    SportsWalking_coef_2 = 0.029
+
+    def __init__(self, action, duration, weight, height):
+        super().__init__(action, duration, weight)
+        self.height = height
+
+    def get_spent_calories(self) -> float:
+        return ((self.SportsWalking_coef_1 * self.weight + (
+                    self.get_mean_speed() ** 2 // self.height) * self.SportsWalking_coef_2 * self.weight) * self.duration * self.MIN_IN_HOUR)
+
+
+class Swimming(Training):
+    """Тренировка: плавание."""
+    LEN_STEP = 1.38
+    Swimming_coef_1 = 1.1
+    Swimming_coef_2 = 2
+
+    def __init__(self, action, duration, weight, length_pool, count_pool):
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+
+    def get_mean_speed(self) -> float:
+        return (self.length_pool * self.count_pool / self.M_IN_KM / self.duration)
+
+    def get_spent_calories(self):
+        return f'{((self.get_mean_speed() + self.Swimming_coef_1) * self.Swimming_coef_2 * self.weight):.3f}'
+
+
+def read_package(workout_type, data) -> Training:
+    """Прочитать данные полученные от датчиков."""
+    workout_types = {'RUN': Running, 'WLK': SportsWalking, 'SWM': Swimming}
+    return workout_types[workout_type](*data)
+
+
+def main(training: Training) -> None:
+    """Главная функция."""
+    info = training.show_training_info()
+    print(info.get_message())
+
+
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
